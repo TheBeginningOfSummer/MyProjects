3728cb18fe7106e4431d9567154c8f80ed79f0b9
May <May@Summer.Afternoon>
2023-01-09 17:09:14 +0800

    添加图形绘制功能

--- MyProjects/OpenCVTool.cs
+++ MyProjects/OpenCVTool.cs
@@ -15,19 +15,27 @@ namespace MyProjects
         public bool IsCaptureImage = false;
         public Action<Mat>? VideoUpdateAction;
         public MouseCallback? MouseCallbackEvent;
+        public int DrawShape = 1;
 
         private Mat img = new Mat();
+        private Window? window;
+        private OpenCvSharp.Point startPoint;
 
         public OpenCVTool()
         {
+            //MouseCallbackEvent += MouseDraw;
             Debug.WriteLine("程序启动");
-            
         }
 
         public void ReadImage(string filePath)
         {
+            MouseCallbackEvent = new MouseCallback(MouseDraw);
+            window = new Window("Input Image");
+            window.SetMouseCallback(MouseCallbackEvent);
+
             img = Cv2.ImRead(filePath);
-            Cv2.ImShow("Input Image", img);
+            //Cv2.ImShow("Input Image", img);
+            window.ShowImage(img);
             Cv2.WaitKey();
             img.Release();
             Cv2.DestroyAllWindows();
@@ -117,10 +125,68 @@ namespace MyProjects
             img.Release();
             Cv2.DestroyAllWindows();
         }
-
-        public void DrawShape()
+        int mm;
+        public void MouseDraw(MouseEventTypes mouseEvent, int x, int y, MouseEventFlags flags, IntPtr userData)
         {
-
+            if (mouseEvent == MouseEventTypes.LButtonDown)
+            {
+                startPoint.X = x;
+                startPoint.Y = y;
+            }
+            else if (mouseEvent == MouseEventTypes.MouseMove && flags == MouseEventFlags.LButton)
+            {
+                OpenCvSharp.Point endPoint;
+                endPoint.X = x;
+                endPoint.Y = y;
+                if (DrawShape == 0)
+                {
+                    Cv2.Line(img, startPoint, endPoint, Scalar.AliceBlue, 1);
+                }
+                else if (DrawShape == 1)
+                {
+                    Cv2.Rectangle(img, startPoint, endPoint, Scalar.AliceBlue, 1);
+                }
+                else if (DrawShape == 2)
+                {
+                    int a = endPoint.X - startPoint.X;
+                    int b = endPoint.Y - startPoint.Y;
+                    int r = (int)Math.Sqrt(a ^ 2 + b ^ 2);
+                    Cv2.Circle(img, startPoint, r, Scalar.AliceBlue, 1);
+                    //Cv2.Polylines
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else if (mouseEvent == MouseEventTypes.LButtonUp)
+            {
+                OpenCvSharp.Point endPoint;
+                endPoint.X = x;
+                endPoint.Y = y;
+                if (DrawShape == 0)
+                {
+                    Cv2.Line(img, startPoint, endPoint, Scalar.AliceBlue, 1);
+                }
+                else if (DrawShape == 1)
+                {
+                    Cv2.Rectangle(img, startPoint, endPoint, Scalar.Green, 1);
+                }
+                else if (DrawShape == 2)
+                {
+                    int a = endPoint.X - startPoint.X;
+                    int b = endPoint.Y - startPoint.Y;
+                    int r = (int)Math.Sqrt(a ^ 2 + b ^ 2);
+                    Cv2.Circle(img, startPoint, r, Scalar.AliceBlue, 1);
+                }
+                else
+                {
+                    return;
+                }
+            }
+            window.ShowImage(img);
+            mm++;
+            Debug.WriteLine("鼠标调用" + mm);
         }
     }
 }
