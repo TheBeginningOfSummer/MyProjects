c3d289fd1fbf5f6c6accd15ef5d0e780df5acb7d
May <May@Summer.Afternoon>
2023-04-14 17:18:40 +0800

    IPNS

---
+++ IPFSVideo/Models/IPNS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPFSVideo.Models
+{
+    public class IPNS
+    {
+        public string? Name { get; set; }
+        public string? Id { get; set; }
+
+        public IPNS()
+        {
+            //System.Text.Json.JsonSerializer.DeserializeAsync<>
+
+        }
+    }
+}
